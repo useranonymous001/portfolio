2337b195da2f193354984bf1061c7b4cdf8e70d8
useranonymous00101 <khatrirohan430@gmail.com>
2024-03-12 00:46:54 +0545

    done nothing

---
+++ main.js
@@ -0,0 +1,12 @@
+// let revealContent = document.querySelector(".nav-bar");
+// let hideContent = document.querySelector(".navbar-hidden");
+
+// const revealNavbar = () => {
+//   if (revealContent.classList.contains("nav-bar")) {
+//     revealContent.classList.remove("nav-bar");
+//   } else {
+//     revealContent.classList.add("nav-bar");
+//   }
+// };
+
+// hideContent.addEventListener("click", revealNavbar);
